c3d4de3bfd2fea3bbe2738034526d1e61e750cf6
lykmapipo <lallyelias87@gmail.com>
2015-08-20 14:52:49 +0300

    initial repository setup for forage query

---
+++ Gruntfile.js
@@ -0,0 +1,152 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Configurable paths for the module
+    var props = {
+        src: 'src',
+        dist: 'dist',
+        test: 'test',
+        spec: 'test/spec',
+        tmp: '.tmp'
+    };
+
+    // Project propsuration.
+    grunt
+        .initConfig({
+            pkg: grunt.file.readJSON('bower.json'),
+            props: props,
+            meta: {
+                banner: [
+                    '/**',
+                    ' * <%= pkg.description %>',
+                    ' * @version v<%= pkg.version %> - <%= grunt.template.today() %>',
+                    ' * @link <%= pkg.homepage %>',
+                    ' * @authors <%= pkg.authors.join(", ") %>',
+                    ' * @license MIT License, http://www.opensource.org/licenses/MIT',
+                    ' */',
+                    ''
+                ].join('\n')
+            },
+
+            //clean environments
+            clean: {
+                dist: {
+                    files: [{
+                        dot: true,
+                        src: [
+                            '.tmp',
+                            '<%= props.dist %>'
+                        ]
+                    }]
+                },
+                server: '.tmp'
+            },
+
+            // Make sure code styles are up to par 
+            // and there are no obvious mistakes
+            jshint: {
+                options: {
+                    jshintrc: '.jshintrc',
+                    reporter: require('jshint-stylish')
+                },
+                src: {
+                    src: [
+                        'Gruntfile.js',
+                        '<%= props.src %>/**/*.js'
+                    ]
+                },
+                test: {
+                    options: {
+                        jshintrc: '<%= props.test %>/.jshintrc'
+                    },
+                    src: ['<%= props.spec %>/**/*.js']
+                },
+                example: {
+                    src: ['<%= props.example %>/**/*.js']
+                }
+            },
+
+            //watch environment
+            watch: {
+                bower: {
+                    files: ['bower.json'],
+                    tasks: ['newer:jshint:src', 'newer:jshint:test', 'karma']
+                },
+                src: {
+                    files: ['<%= props.src %>/**/*.js'],
+                    tasks: ['newer:jshint:src', 'karma'],
+                },
+                test: {
+                    files: ['<%= props.test %>/karma.conf.js', '<%= props.spec %>/**/*.js'],
+                    tasks: ['newer:jshint:test', 'karma']
+                },
+                gruntfile: {
+                    files: ['Gruntfile.js'],
+                    tasks: ['newer:jshint:src', 'newer:jshint:test', 'karma']
+                }
+            },
+            concat: {
+                options: {
+                    banner: '<%= meta.banner %>\n',
+                    stripBanners: true,
+                    separator: '\n\n'
+                },
+                dist: {
+                    files: {
+                        'dist/<%= pkg.name %>.js': []
+                    }
+                }
+            },
+
+            // Automatically inject Bower components into the app
+            wiredep: {
+                test: {
+                    devDependencies: true,
+                    src: '<%= karma.unit.configFile %>',
+                    ignorePath: /\.\.\//,
+                    fileTypes: {
+                        js: {
+                            block: /(([\s\t]*)\/{2}\s*?bower:\s*?(\S*))(\n|\r|.)*?(\/{2}\s*endbower)/gi,
+                            detect: {
+                                js: /'(.*\.js)'/gi
+                            },
+                            replace: {
+                                js: '\'{{filePath}}\','
+                            }
+                        }
+                    }
+                }
+            },
+
+            //karma task configuration
+            karma: {
+                unit: {
+                    configFile: '<%= props.test %>/karma.conf.js',
+                    singleRun: true
+                }
+            }
+        });
+
+    //custom tasks
+    grunt.registerTask('default', ['watch']);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'jshint',
+        'karma',
+        'concat'
+    ]);
+
+    grunt.registerTask('test', [
+        'wiredep',
+        'karma'
+    ]);
+
+};
---
+++ README.md
@@ -0,0 +1,80 @@
+forageQuery
+===========
+[![Build Status](https://travis-ci.org/lykmapipo/forageQuery.svg?branch=master)](https://travis-ci.org/lykmapipo/forageQuery)
+
+Query builder for [localForage](https://github.com/mozilla/localForage)
+
+## Guide
+
+* [Install](#install)
+* [Usage](#usage)
+* [Testing](#testing)
+* [Development](#development)
+* [Contribute](#contribute)
+
+
+
+## Install
+```sh
+$ bower install --save forage-query
+```
+## Usage
+- Include `forageQuery` into your app index.html 
+```html
+<!doctype html>
+<html>
+<head>
+    ...
+</head>
+<body>
+    
+    ...
+
+    <!-- build:js(.) scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="bower_components/forage-query/dist/forageQuery.js"></script>
+    <!-- endbower -->
+    <!-- endbuild -->
+
+    <!-- build:js({.tmp,app}) scripts/yourApp.js -->
+    <script src="scripts/app.js"></script>
+    <!-- endbuild -->
+
+    ...
+
+</body>
+</html>
+```
+
+## Testing
+* Clone this repository
+
+* Install all development dependencies
+```sh
+$ npm install --dev && bower install
+```
+
+* Then run test
+```sh
+$ npm test
+```
+
+## Development
+`forageQuery` has set of useful `grunt` tasks to help you with development. By running
+```sh
+$ grunt
+```
+`forageQuery` will be able watch your development environment for file changes and apply `jshint` and `karma` to the project.
+
+## Contribute
+Fork this repo and push in your ideas. Do not forget to add a bit of test(s) of what value you adding.
+
+## Licence
+
+Copyright (c) 2015 lykmapipo & Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
