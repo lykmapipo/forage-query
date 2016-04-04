e53a0acccb25413fed9f402866bea98b9552879b
lykmapipo <lallyelias87@gmail.com>
2016-04-04 14:13:37 +0300

    update findOneAndUpdate specs

---
+++ test/spec/updators/findOneAndUpdate.js
@@ -0,0 +1,73 @@
+'use strict';
+
+
+describe('Query#findOneAndUpdate', function() {
+
+    var items;
+
+    before(function(done) {
+        localforage.create(this.items(), function(error, createdItems) {
+            items = createdItems;
+            done(error, createdItems);
+        });
+    });
+
+    it('should be able to remove an item using callback style', function(done) {
+        var update = {
+            name: faker.name.findName()
+        };
+
+        localforage.findOneAndUpdate({
+            name: items[0].name
+        }, update, function(error, updatedItem) {
+            expect(error).to.not.exist;
+            expect(updatedItem).to.exist;
+            expect(updatedItem.name).to.equal(update.name);
+            expect(updatedItem.id).to.equal(items[0].id);
+            expect(updatedItem.name).to.not.equal(items[0].name);
+            done(error, updatedItem);
+        });
+    });
+
+    it('should be able to remove items using deffered style', function(done) {
+        var update = {
+            name: faker.name.findName()
+        };
+
+        localforage.findOneAndUpdate({
+            name: items[1].name
+        }, update).exec(function(error, updatedItem) {
+            expect(error).to.not.exist;
+            expect(updatedItem).to.exist;
+            expect(updatedItem.name).to.equal(update.name);
+            expect(updatedItem.id).to.equal(items[1].id);
+            expect(updatedItem.name).to.not.equal(items[1].name);
+            done(error, updatedItem);
+        });
+    });
+
+    it('should be able to remove items using promise style', function(done) {
+        var update = {
+            name: faker.name.findName()
+        };
+        localforage.findOneAndUpdate({
+            name: items[2].name
+        }, update).then(function(updatedItem) {
+            expect(updatedItem).to.exist;
+            expect(updatedItem.name).to.equal(update.name);
+            expect(updatedItem.id).to.equal(items[2].id);
+            expect(updatedItem.name).to.not.equal(items[2].name);
+            done(null, updatedItem);
+        }).catch(done);
+    });
+
+
+    after(function(done) {
+        localforage.remove({
+            id: {
+                $in: _.map(items, '_id')
+            }
+        }, done);
+    });
+
+});
